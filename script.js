5060a223a31a22142c9556d8ac38c2531ef8dc89
fediker-eng <FedBlend@tutamail.com>
2025-06-19 03:28:35 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+document.getElementById("myButton").onclick = function() {
+    document.getElementById("myModal").style.display = "block";
+
+    };
+document.getElementById("myButton1").onclick = function() {
+    document.getElementById("myModal1").style.display = "flex";
+
+};
+
+
+document.getElementById("myButton2").onclick = function() {
+    document.getElementById("myModal2").style.display = "block";
+
+};
+document.getElementById("myButton3").onclick = function() {
+    document.getElementById("myModal3").style.display = "block";
+};
+//image window
+
+document.getElementById("image").onclick = function() {
+    document.getElementById("myModal5").style.display = "block";
+};
+document.getElementById("myButton4").onclick = function() {
+    let userInput = prompt("Enter your Name or Email");
+    if(userInput !== null) {
+        alert("I'm is blender artist. I can modeling 3d objects. You can download one my model or download all models for free.");
+
+    }
+};
+document.getElementById("myButton5").onclick = function() {
+    document.getElementById("myModal4").style.display = "flex";
+};
+  //my portfolio
+document.getElementById("close-button").onclick = function() {
+    document.getElementById("myModal").style.display = "none";
+};
+//my big project
+document.getElementById("close-button2").onclick = function() {
+    document.getElementById("myModal2").style.display = "none";
+
+};
+//realistic model
+document.getElementById("close-button1").onclick = function() {
+    document.getElementById("myModal1").style.display = "none";
+
+};
+document.getElementById("close-button3").onclick = function() {
+    document.getElementById("myModal3").style.display = "none";
+};
+
+document.getElementById("myButton6").onclick = function() {
+    document.getElementById("myModal4").style.display = "none";
+
+};
+//image window
+document.getElementById("close-button4").onclick = function() {
+    document.getElementById("myModal5").style.display = "none";
+
+};
+
+
+
+
+
+
+
+
+
+
